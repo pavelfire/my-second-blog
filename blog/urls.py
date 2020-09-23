e21aad2de942a318e19e58db21c43f272db56b28
Pavel i5 Tim <fire_ra@bk.ru>
2020-09-23 20:06:52 +0300

    Changed html file

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('landing', views.landing, name='landing_page'),
+    path('landing/', views.landing, name='landing_page'),
 ]
